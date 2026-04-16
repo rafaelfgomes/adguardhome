456312aa2ad2e061a9717ce760fbba95b056f242
Rafael Ferreira Gomes <faelfg@gmail.com>
2026-04-16 19:23:06 -0300

    fix adguard first execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,28 @@ chown -R adguard:adguard /opt/adguardhome/conf
 
 chown -R adguard:adguard /opt/adguardhome/work
 
-setcap 'cap_net_bind_service=+eip' /opt/AdGuardHome/AdGuardHome
-
 echo ">> Iniciando a aplicação..."
 
-exec su-exec adguard "$@"
+if [ ! -f "/opt/adguardhome/conf/AdGuardHome.yaml" ]; then
+    echo "================================================================="
+    echo ">> [AVISO] Primeira execução detectada!"
+    echo ">> O Wizard inicial do AdGuard Home exige root por padrão."
+    echo ">> Iniciando temporariamente como ROOT..."
+    echo ">>"
+    echo ">> O QUE VOCÊ DEVE FAZER AGORA:"
+    echo ">> 1. Acesse o navegador na porta 3000 e finalize a instalação."
+    echo ">> 2. Volte ao terminal e reinicie: docker compose restart"
+    echo "================================================================="
+
+    # Executa como root (sem su-exec) apenas nesta vez
+    exec "$@"
+else
+    echo ">> Configuração encontrada! Iniciando de forma segura."
+    echo ">> Rebaixando privilégios para UID: $PUID e GID: $PGID"
+
+    # Dá permissão na porta 53 para o arquivo
+    setcap 'cap_net_bind_service=+eip' /opt/AdGuardHome/AdGuardHome
+
+    # Executa travado como usuário comum
+    exec su-exec adguard "$@"
+fi
